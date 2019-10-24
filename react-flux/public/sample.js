1288ab40be4e62e70bdb1c09688443a763a120af
Gandhi <rishabh.gandhi@metronom-external.com>
2019-10-24 16:43:35 +0530

    react-flux structure changes

---
+++ react-flux/public/sample.js
@@ -0,0 +1,19 @@
+let data = [{
+        id: 1,
+        pdtName: "Gamia Laptop",
+        pdtPrice: "33000",
+        pdtDescription: "An excellent choice for an awesome gaming experience",
+        img: "Images / laptop.jpg",
+        rating: "3",
+        isDiscontinued: false
+    },
+    {
+        id: 2,
+        pdtName: "Lenovo Laptop",
+        pdtPrice: "35000",
+        pdtDescription: "An excellent choice for development",
+        img: "Images / laptop.jpg",
+        rating: "4",
+        isDiscontinued: false
+    }
+]
